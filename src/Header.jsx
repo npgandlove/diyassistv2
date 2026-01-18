6883d754aedb8381c9363d6c40c51bbae908bba0
npgandlove <james.mcintyre4@gmail.com>
2026-01-17 23:15:57 -0500

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,1 +1,22 @@
 
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from 'src/DIY Assist v10.png'; // Make sure to add your logo image file
+
+const Header = () => {
+  return (
+    <header className="app-header">
+      <Link to="/">
+        <img src={logo} alt="Home" className="app-logo" />
+      </Link>
+      <nav>
+        {/* You can add other navigation links here */}
+        {/* <Link to="/Providers">Providers</Link> */}
+        
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
+
